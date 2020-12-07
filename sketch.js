7830acd09b7ca1d55dd96e42b4ffc8d91a951c4c
jabez0106 <73861708+jabez0106@users.noreply.github.com>
2020-12-08 01:23:15 +0900

    블랙홀 가져오기
    지난 2주간 만들었던 블랙홀을 그대로 가져왔다.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
 function setup() {
   createCanvas(1920, 1080);
+  blackhole = new Blackhole();
 }
 
 function draw() {
   background(20);
+  blackhole.display();
+  blackhole.scaleBounce();
+}
+
+class Blackhole {
+  constructor() {
+  }
+
+  display() {
+    fill(0);
+    strokeWeight(20);
+    stroke('rgba(100,0,100,100)');
+    translate(300,300)
+  }
+
+  scaleBounce() {
+    if (keyIsPressed === true) {
+      let step = frameCount % 120;
+      applyMatrix(step / 100, 0, 0, step / 100, 0, 0);
+      circle(0, 0, 600);
+    } else {
+      let step = frameCount % 20;
+      applyMatrix(1 / step, 0, 0, 1 / step, 0, 0);
+    }
+  }
 }
