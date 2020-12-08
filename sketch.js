90f6fa18785a5178f1e50cd3d25bd83911f537e1
jabez0106 <73861708+jabez0106@users.noreply.github.com>
2020-12-09 07:29:24 +0900

    블랙홀 사이즈 변경
    화면을 다 뒤덮는 블랙홀로 크기를 두 배 키워줌

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ class Blackhole {
     if (keyIsPressed === true) {
       let step = frameCount % 120;
       applyMatrix(step / 100, 0, 0, step / 100, 0, 0);
-      circle(0, 0, 900);
+      circle(0, 0, 1800);
     } else {
       let step = frameCount % 20;
       applyMatrix(1 / step, 0, 0, 1 / step, 0, 0);
