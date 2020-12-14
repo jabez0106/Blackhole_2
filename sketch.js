cbdb6315f817a64c9f0119e79935aa0063766740
jabez0106 <73861708+jabez0106@users.noreply.github.com>
2020-12-15 07:45:09 +0900

    스케치 파일 분리
    블랙홀, 배경 파티클 파일 분리

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 let cn;
-var bg_p;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -25,28 +24,4 @@ function draw() {
   blackhole.scaleBounce();
 
   bg_p.show();
-
-}
-
-class Blackhole {
-  constructor() {
-  }
-
-  display() {
-    fill(0);
-    strokeWeight(0);
-    stroke('rgba(100,0,100,100)');
-    translate(960,540)
-  }
-
-  scaleBounce() {
-    if (keyIsPressed === true) {
-      let step = frameCount % 120;
-      applyMatrix(step / 100, 0, 0, step / 100, 0, 0);
-      circle(0, 0, 1800);
-    } else {
-      let step = frameCount % 20;
-      applyMatrix(1 / step, 0, 0, 1 / step, 0, 0);
-    }
-  }
 }
