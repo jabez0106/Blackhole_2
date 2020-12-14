f73a003d168fc54752cc3a26ae7034890850cd01
jabez0106 <73861708+jabez0106@users.noreply.github.com>
2020-12-15 07:42:43 +0900

    배경 파티클 파일 분리
    임의의 값을 적용하여 다시 파일 분리

--- bg_particle.js
+++ bg_particle.js
@@ -1,8 +1,8 @@
 class bg_particle {
   constructor() {
-    this.pos = createVector(x,y)
-    this.vel = createVector(x,y)
-    this.acc = createVector(x,y)
+    this.pos = createVector(0,0);
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
   }
   show() {
     ellipse(this.pos.x, this.pos.y, 10);
